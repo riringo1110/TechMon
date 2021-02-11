42b64521125d7574ef7f85cc8aac1c4bae2e4f32
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-11 17:54:50 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by Ririko Nagaishi on 2021/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
